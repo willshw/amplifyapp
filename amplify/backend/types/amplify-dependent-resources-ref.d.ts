dd8ca9bb9475d5aa5ac78780cc3f318082054f42
William Wang <shanhewang@gmail.com>
2022-01-04 23:54:23 -0800

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp3ab1dcb8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>We now have Auth!</h1>
+        </header>
+      </div>
+    </Authenticator>
   );
 }
 
